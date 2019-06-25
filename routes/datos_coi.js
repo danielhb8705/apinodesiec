e7f10d0bd7ed96efffa764746ba6f25a51fbed49
danielhb8705 <danielhb8705@gmail.com>
2019-06-25 19:56:24 -0400

    ER X CC x DPTOS 25-06-2019

--- routes/datos_coi.js
+++ routes/datos_coi.js
@@ -20,6 +20,7 @@ router.post('/getDepartamentos', function(req, res, next) {
 router.post('/getDatosCuentas', function(req, res, next) {
 	var mes = req.body.month;
 	var anhio = req.body.anhio;
+	
 	//console.log(mes);
 	datosCOI.getDatosCuentas(mes,anhio)
     .then((response) => {
@@ -39,6 +40,42 @@ router.post('/getDatosCuentasXDpto', function(req, res, next) {
 	var anhio = req.body.anhio;
 	//console.log(mes);
 	datosCOI.getDatosCuentasXDpto(mes,anhio)
+    .then((response) => {
+		
+		res.status(200).json(response);
+		
+
+    })
+    .catch((error) => {
+        res.status(500).json(error);
+    });
+
+});
+//Obtener Datos Ventas por departamentos para EC por CC RUBRO 6 (Ventas/Ingresos)
+router.post('/getDatosVentasXDptoParaERXCC', function(req, res, next) {
+	var mes = req.body.month;
+	var anhio = req.body.anhio;
+	var dpto = req.body.dpto;
+	//console.log(mes+" "+dpto);
+	datosCOI.getVentasXdepartamentos(dpto)
+    .then((response) => {
+		
+		res.status(200).json(response);
+		
+
+    })
+    .catch((error) => {
+        res.status(500).json(error);
+    });
+
+});
+//Obtener Datos Gastos Generales por departamentos para EC por CC RUBRO 7 (Gastos Generales)
+router.post('/getDatosGastosGeneralesXDptoParaERXCC', function(req, res, next) {
+	var mes = req.body.month;
+	var anhio = req.body.anhio;
+	var dpto = req.body.dpto;
+	//console.log(mes);
+	datosCOI.getGastosGeneralesXdepartamentos(dpto)
     .then((response) => {
 		
 		res.status(200).json(response);
